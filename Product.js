647a0df1dc0d6595a2f21cabd79e4b47d580e4da
MOHAN <mohanmohan200405@gmail.com>
2026-06-15 03:44:18 +0530

    Add product data to Product.js

---
+++ Product.js
@@ -0,0 +1,30 @@
+const products = [
+  {
+    id: 1,
+    name: "Oversized T-Shirt",
+    price: 899,
+    category: "Men",
+    image:
+      "https://images.unsplash.com/photo-1521572163474"
+  },
+
+  {
+    id: 2,
+    name: "Hoodie",
+    price: 1499,
+    category: "Winter",
+    image:
+      "https://images.unsplash.com/photo-1503341504253"
+  },
+
+  {
+    id: 3,
+    name: "Cargo Pant",
+    price: 1299,
+    category: "Streetwear",
+    image:
+      "https://images.unsplash.com/photo-1515886657613"
+  }
+];
+
+export default products;
