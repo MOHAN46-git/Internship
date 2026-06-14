23348f3296612c98a341d4d2f8e675bc91b2bb34
MOHAN <mohanmohan200405@gmail.com>
2026-06-15 03:50:07 +0530

    Add About component for 7One Clothing

---
+++ About.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function About() {
+
+ return (
+
+  <div className="container">
+
+   <h2>
+    About 7One Clothing
+   </h2>
+
+   <p>
+    7One Clothing is a
+    modern fashion brand
+    offering premium
+    streetwear collections
+    designed for today's
+    generation.
+   </p>
+
+  </div>
+
+ );
+}
+
+export default About;
